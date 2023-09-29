3c776a382ea24b49a57351cfd07bacba140ebd70
Keivan Kazemi <kaze0017@algonquinlive.com>
2023-09-29 14:45:34 -0400

    adds lists classes

--- src/js/main.js
+++ src/js/main.js
@@ -66,3 +66,59 @@ function copyText() {
   // Deselect the text
   inputField.setSelectionRange(0, 0);
 }
+
+//
+//
+// Lists Functions
+
+let group1Lists = document.querySelectorAll(".li-color");
+let group2Lists = document.querySelectorAll(".li-style");
+
+// Get the input element for the generated string
+let generatedLiClass = document.getElementById("generatedLiClass");
+
+// Add event listeners to both groups of radio buttons
+group1Lists.forEach(function (button) {
+  button.addEventListener("change", generateStringLi);
+});
+
+group2Lists.forEach(function (button) {
+  button.addEventListener("change", generateStringLi);
+});
+
+// Function to generate the string based on selected values
+function generateStringLi() {
+  let group1Value = getValueOfSelectedRadio(group1Lists);
+  let group2Value = getValueOfSelectedRadio(group2Lists);
+  console.log("click");
+
+  // Create the string based on selected values
+  let generatedString = `list-item${
+    group2Value !== null ? `-${group2Value}` : ""
+  }${group1Value !== null ? `-${group1Value}` : ""}`;
+
+  // Update the input element with the generated string
+  generatedLiClass.value = generatedString;
+  document.getElementById("sampleLi").setAttribute("class", generatedString);
+}
+
+// const inputField = document.getElementById("btnText");
+const updateLi = document.getElementById("sampleLi");
+
+// Add a keyup event listener to the input field
+
+const liCopyButton = document.getElementById("liCopyButton");
+liCopyButton.addEventListener("click", copyText);
+function copyText() {
+  // Get the input field
+  let inputField = document.getElementById("generatedLiClass");
+
+  // Select the text in the input field
+  inputField.select();
+
+  // Copy the selected text to the clipboard
+  document.execCommand("copy");
+
+  // Deselect the text
+  inputField.setSelectionRange(0, 0);
+}
